2a363180b610ba6c1fb2cfcbfc15a69d3b3a582d
Cameron Reid <csreid.cr@gmail.com>
2017-11-21 15:43:35 -0500

    add lots more filtering power

--- test/filter.js
+++ test/filter.js
@@ -1,7 +1,7 @@
 let assert = require('assert');
 
 let squel = require('squel');
-let Filter = require('../lib/Filter');
+let Filter = require('../lib/classes/Filter');
 
 let q = squel.select()
 	.from('clients');
@@ -11,13 +11,27 @@ let foo = new Filter({
 });
 
 foo.applyToSql(q);
+assert(q.toString() === 'SELECT * FROM clients WHERE (client_id = 1)', q.toString())
 
-let func = foo.toJsFilter();
+q = squel.select()
+	.from('clients');
+foo = new Filter({
+	name: 'TeamCo',
+	customer_count: {
+		$gt: 5,
+		$lt: 10
+	},
+	employee_count: {
+		$gte: 100,
+		$lte: 499
+	},
+	archived: {
+		$ne: null
+	},
+	orders: {
+		$in: [1,3,5,11]
+	}
+});
+foo.applyToSql(q);
 
-assert(func({
-	client_id: 1
-}));
-
-assert(!func({
-	client_id: 2
-}));
+assert(q.toString() === 'SELECT * FROM clients WHERE (name = TeamCo) AND (customer_count > 5) AND (customer_count < 10) AND (employee_count >= 100) AND (employee_count <= 499) AND (archived != null) AND (orders IN 1,3,5,11)', 'Complex filters failed');
