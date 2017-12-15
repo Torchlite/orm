19929fc72ef1b638fa17b5d73ec9b504cd280900
Cameron Reid <csreid.cr@gmail.com>
2017-12-15 12:55:08 -0500

    remove fromSQLRow requirement

--- test/unit/AssociateTest.js
+++ test/unit/AssociateTest.js
@@ -60,8 +60,6 @@ let g = new Game({ gameId: 1, date: '2017-08-15' });
 let t = new Team({ teamId: 1, name: 'Crushinators' });
 let u = new User({ userId: 1, name: 'John', teamId: 10 });
 
-console.log(t.genSql(User));
-
 assert(u.genSql(Team) === 'SELECT teams.team_id, teams.name FROM teams WHERE (team_id = 10)', 'oneToMany failed');
 assert(t.genSql(User) === 'SELECT users.user_id, users.team_id, users.name FROM users WHERE (team_id = 1)', 'manyToOne failed');
 assert(t.genSql(Game) === 'SELECT games.game_id, games.date FROM games INNER JOIN game_teams ON (games.game_id = game_teams.game_id) WHERE (team_id=1)', 'manyToMany failed');
