cc37a217561c2110f6bae5dbad55c8a037529b50
Cameron Reid <csreid.cr@gmail.com>
2017-12-15 16:34:53 -0500

    use save for creates and updates

--- test/unit/ModelTest.js
+++ test/unit/ModelTest.js
@@ -10,5 +10,7 @@ let {
 } = require('./bootstrap');
 
 let user = new User({ id: null, name: 'Cameron', teamId: 10 });
+let user2 = new User({ id: 2, name: 'Cameron', teamId: 10 });
 
-assert(user.saveSql() === `INSERT INTO users (name, team_id) VALUES ('Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10`, 'Save query is wrong');
+assert(user.saveSql() === `INSERT INTO users (name, team_id) VALUES ('Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10`, 'Create query is wrong');
+assert(user2.saveSql() === `INSERT INTO users (user_id, name, team_id) VALUES (2, 'Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10`, `Update query is wrong: ${user2.saveSql()}`);
