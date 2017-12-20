186a554a32ad25a477846bd8426da9c424cc3ab8
Cameron Reid <csreid.cr@gmail.com>
2017-12-20 16:14:00 -0500

    test for filter on association gets

--- test/unit/AssociateTest.js
+++ test/unit/AssociateTest.js
@@ -61,7 +61,11 @@ assert(u._getSql(Team) === 'SELECT teams.team_id, teams.name FROM teams WHERE (t
 assert(t._getSql(User) === 'SELECT users.user_id, users.name, users.team_id, users.created_at FROM users WHERE (team_id = 1)', 'oneToMany get failed: ' + t._getSql(User));
 assert(t._getSql(Game) === 'SELECT games.game_id, games.date FROM games INNER JOIN game_teams ON (games.game_id = game_teams.game_id) WHERE (team_id=1)', `manyToMany get failed: ${t._getSql(Game)}`);
 
-console.log(u._getSql(Team, new Filter({ name: 'Foobar' })));
+let f = new Filter({ name: 'a name' });
+
+assert(u._getSql(Team, f) === `SELECT teams.team_id, teams.name FROM teams WHERE (team_id = 10) AND (name = 'a name')`, u._getSql(Team, f));
+assert(t._getSql(User, f) === `SELECT users.user_id, users.name, users.team_id, users.created_at FROM users WHERE (team_id = 1) AND (name = 'a name')`, t._getSql(User, f));
+assert(t._getSql(Game, f) === `SELECT games.game_id, games.date FROM games INNER JOIN game_teams ON (games.game_id = game_teams.game_id) WHERE (team_id=1) AND (name = 'a name')`, t._getSql(Game, f));
 
 assert(u._addSql(new Team({ teamId: 3 })) === 'UPDATE users SET team_id = 3 WHERE (user_id = 1)', `manyToOne add failed: ${u._addSql(new Team({ teamId: 3 }))}`);
 assert(t._addSql(new User({ id: 5 })) === 'UPDATE users SET team_id = 1 WHERE (user_id = 5)', `oneToMany add failed: ${t._addSql(new User({ id: 5 }))}`);
