360e83d75db665ebfbd72cb9b7e2ab1c793334a9
Cameron Reid <csreid.cr@gmail.com>
2017-12-22 11:30:09 -0500

    Add `fetch` and `fromId`

--- test/unit/ModelTest.js
+++ test/unit/ModelTest.js
@@ -12,3 +12,5 @@ assert(user._saveSql() === `INSERT INTO users (user_id, team_id, name) VALUES (D
 assert(user2._saveSql() === `INSERT INTO users (user_id, team_id, name) VALUES (2, 10, 'Cameron') ON CONFLICT (user_id) DO UPDATE SET team_id = 10, name = 'Cameron' RETURNING user_id, team_id, name`, `Update query is wrong: ${user2._saveSql()}`);
 
 assert(user2._updateSql({ name: 'John' }) === `UPDATE users SET name = 'John' WHERE (user_id = 2) RETURNING user_id, team_id, name`, `Update query is wrong: ${user2._updateSql({ name: 'John' })}`);
+
+assert(user2._fetchSql() === `SELECT users.user_id, users.team_id, users.name FROM users WHERE (user_id = 2)`);
