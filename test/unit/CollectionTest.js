f8e082479b58a29cbfca5ebb1c7dd24809f978df
Cameron Reid <csreid.cr@gmail.com>
2018-01-02 09:49:10 -0500

    add `order` and some comments

--- test/unit/CollectionTest.js
+++ test/unit/CollectionTest.js
@@ -23,11 +23,12 @@ let games = new GameCollection()
 		}
 	})
 	.limit(10)
-	.offset(55);
+	.offset(55)
+	.order('gameId', 'desc');
 
 assert(users.toSql() === 'SELECT user_id, team_id, name FROM users', 'Simple unfiltered returned ' + users.toSql());
 assert(camerons.toSql() === `SELECT user_id, team_id, name FROM users WHERE (name = 'Cameron')`, 'Simple filter returned ' + camerons.toSql());
-assert(games.toSql() === `SELECT game_id, team_id, date FROM games WHERE (date > '2017-01-01' AND date < '2017-01-31' AND winner = 2 AND valid != FALSE) LIMIT 10 OFFSET 55`, 'Complex filter failed: ' + games.toSql());
+assert(games.toSql() === `SELECT game_id, team_id, date FROM games WHERE (date > '2017-01-01' AND date < '2017-01-31' AND winner = 2 AND valid != FALSE) ORDER BY game_id DESC LIMIT 10 OFFSET 55`, 'Complex filter failed: ' + games.toSql());
 
-assert(users.countSql() === 'SELECT count(*) FROM users', users.countSql());
-assert(camerons.countSql() === `SELECT count(*) FROM users WHERE (name = 'Cameron')`, users.countSql());
+assert(users._countSql() === 'SELECT count(*) FROM users', users._countSql());
+assert(camerons._countSql() === `SELECT count(*) FROM users WHERE (name = 'Cameron')`, users._countSql());
