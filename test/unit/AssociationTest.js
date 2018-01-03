d4ccc2318c10ce29d1fc1ee5f21c7ff5672c83d0
Cameron Reid <csreid.cr@gmail.com>
2018-01-03 13:25:22 -0500

    lots of goodies

--- test/unit/AssociationTest.js
+++ test/unit/AssociationTest.js
@@ -1,9 +1,8 @@
 let assert = require('assert');
 
-let { User, Team } = require('../bootstrap');
+let { User, Team, Game, Table } = require('../bootstrap');
 
 class Owner extends User {}
-// Basic oneToMany
 Team.hasMany(User, {
 	key: 'teamId',
 	references: 'teamId'
@@ -19,6 +18,25 @@ Team.hasOne(Owner, {
 	references: 'id'
 }, 'owner');
 
+Team.belongsToMany(Game, {
+	through: new Table('team_games', {
+		game_id: {
+			type: 'integer'
+		},
+		team_id: {
+			type: 'integer'
+		}
+	}),
+	leftKey: {
+		key: 'teamId',
+		references: 'team_id'
+	},
+	rightKey: {
+		key: 'gameId',
+		references: 'game_id'
+	}
+});
+
 let t = new Team({ teamId: 1 });
 let user = new User({
 	userId: 1,
@@ -32,8 +50,12 @@ let collection = t.related(User)
 
 let userTeam = user.related(Team);
 
-assert(collection.toSql() === 'SELECT user_id, name, team_id, created_at FROM users WHERE (team_id = 1 AND name = \'John\')');
-
+assert(collection.toSql() === 'SELECT users.user_id, users.name, users.team_id, users.created_at FROM users WHERE (team_id = 1 AND name = \'John\')', collection.toSql());
 assert(typeof userTeam.cheer === 'function');
 assert(typeof userTeam.teamId === 'number', `Team id type: ${typeof userTeam.teamId}`);
 assert(userTeam.teamId === 10, `Team id: ${userTeam.teamId}`);
+
+console.log(Object.keys(t));
+console.log(t.users.toSql());
+
+assert(userTeam.related(Game).toSql() === 'SELECT games.game_id, games.date, team_games.game_id AS __context_game_id, team_games.team_id AS __context_team_id FROM games INNER JOIN team_games ON (games.game_id = team_games.game_id) INNER JOIN teams ON (teams.team_id = team_games.team_id) WHERE (teams.team_id = 10)')
