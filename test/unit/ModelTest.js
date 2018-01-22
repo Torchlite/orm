0d2f65ca62a6b0c14d494bd53c13255492b427f1
Cameron Reid <csreid.cr@gmail.com>
2018-01-22 13:34:00 -0500

    qualify values in `returning` clauses

--- test/unit/ModelTest.js
+++ test/unit/ModelTest.js
@@ -8,8 +8,8 @@ let {
 let user = new User({ name: 'Cameron', teamId: 10 });
 let user2 = new User({ userId: 2, name: 'Cameron', teamId: 10 });
 
-let expectedSave = `INSERT INTO users (name, team_id) VALUES ('Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10 RETURNING user_id, name, team_id, created_at`;
-let expectedUpdate = `INSERT INTO users (user_id, name, team_id) VALUES (2, 'Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10 RETURNING user_id, name, team_id, created_at`;
+let expectedSave = `INSERT INTO users (name, team_id) VALUES ('Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10 RETURNING users.user_id, users.name, users.team_id, users.created_at`;
+let expectedUpdate = `INSERT INTO users (user_id, name, team_id) VALUES (2, 'Cameron', 10) ON CONFLICT (user_id) DO UPDATE SET name = 'Cameron', team_id = 10 RETURNING users.user_id, users.name, users.team_id, users.created_at`;
 
 assert(user._saveSql() === expectedSave, `Create query is wrong:
 	${user._saveSql()}
@@ -20,14 +20,27 @@ assert(user2._saveSql() === expectedUpdate, `Update query 1 is wrong:
 	${expectedUpdate}
 `);
 
-assert(user2._updateSql({ name: 'John' }) === `UPDATE users SET name = 'John' WHERE (user_id = 2) RETURNING user_id, team_id, name, created_at`, `Update query is wrong: ${user2._updateSql({ name: 'John' })}`);
-assert(user2._fetchSql() === `SELECT users.user_id, users.team_id, users.name, users.created_at FROM users WHERE (user_id = 2)`);
+let expectedUpdate2 = `UPDATE users SET name = 'John' WHERE (user_id = 2) RETURNING users.user_id, users.team_id, users.name, users.created_at`
 
-let sql1 = user._cloneSql({ userId: '__DEFAULT', createdAt: '__DEFAULT' });
+assert(user2._updateSql({ name: 'John' }) === expectedUpdate2, `Update query is wrong:
+	${user2._updateSql({ name: 'John' })}
+	${expectedUpdate2}
+`);
+
+let expectedFetch = `SELECT users.user_id, users.team_id, users.name, users.created_at FROM users WHERE (user_id = 2)`;
+assert(user2._fetchSql() === expectedFetch, `Fetch SQL was wrong:
+	${user2._fetchSql()}
+	${expectedFetch}
+`);
+
+let expectedClone1Sql = `INSERT INTO users (name, team_id, user_id, created_at) VALUES ('Cameron', 10, DEFAULT, DEFAULT) RETURNING users.user_id, users.name, users.team_id, users.created_at`;
+let expectedClone2Sql = `INSERT INTO users (name, team_id, user_id) VALUES ('Cameron', 10, DEFAULT) RETURNING users.user_id, users.name, users.team_id, users.created_at`;
+
+let cloneSql1 = user._cloneSql({ userId: '__DEFAULT', createdAt: '__DEFAULT' });
 let cloneSql2 = user._cloneSql();
 
-assert(sql1 === `INSERT INTO users (name, team_id, user_id, created_at) VALUES ('Cameron', 10, DEFAULT, DEFAULT) RETURNING user_id, name, team_id, created_at`, `Clone SQL is wrong: ${sql1}`);
-assert(cloneSql2 === `INSERT INTO users (name, team_id, user_id) VALUES ('Cameron', 10, DEFAULT) RETURNING user_id, name, team_id, created_at`, `Clone SQL (no argument) is wrong: ${cloneSql2}`);
+assert(cloneSql1 === expectedClone1Sql, `Clone SQL is wrong: ${cloneSql1}`);
+assert(cloneSql2 === expectedClone2Sql, `Clone SQL (no argument) is wrong: ${cloneSql2}`);
 
 assert.deepEqual(user.toJSON(), { name: 'Cameron', teamId: 10 })
 assert.deepEqual(user2.toJSON(), { userId: 2, name: 'Cameron', teamId: 10 })
