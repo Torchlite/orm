2d0a402114724f3046407121128c0ba7c555cbda
Cameron Reid <csreid.cr@gmail.com>
2018-05-30 14:05:56 -0400

    more tests

--- test/integration/Collection.test.js
+++ test/integration/Collection.test.js
@@ -19,4 +19,57 @@ describe('Collection', () => {
 				done();
 			});
 	});
+
+	describe('#findOne', () => {
+		it('should return an instance', (done) => {
+			users.findOne()
+				.then(u => {
+					/Hello, .*/.test(u.greet()).should.be.equal(true);
+					done();
+				})
+				.catch(done);
+		});
+
+		it('should work with a filter', (done) => {
+			users.findOne({ teamId: 2 })
+				.then(u => {
+					u.teamId.should.be.equal(2);
+					done();
+				})
+				.catch(done);
+		})
+	});
+
+	describe('#findById', async () => {
+		it ('should return the correct user', (done) => {
+			let base;
+			let test;
+
+			users.findOne()
+				.then(u => {
+					base = u;
+
+					return users.findById(base.userId);
+				})
+				.then(tu => {
+					tu.userId.should.equal(base.userId);
+					tu.name.should.equal(base.name);
+					done();
+				})
+				.catch(done);
+		});
+	});
+
+	describe('#remove', async () => {
+		it('should throw', (done) => {
+			users.findOne()
+				.then(u => users.remove(u))
+				.then(() => {
+					throw new Error('Should have failed');
+					done('Failed');
+				})
+				.catch(e => e.message.should.equal('Not implemented; use `delete` in the instance.'))
+				.then(() => done());
+		});
+	});
 });
